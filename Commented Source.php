9afeb9787d83d6a36121633e52e4f2f704ebe4f1
Pat Hawks <pat@pathawks.com>
2013-08-10 22:31:35 -0400

    Shaved six bytes from tinywp.php

--- Commented Source.php
+++ Commented Source.php
@@ -2,15 +2,19 @@
 
 // We're going to replace some common strings with variables to save space. This way, we can keep this file as small as possible
 
+$h='http://';
 $w='wordpress';
 $z='.zip';
+
+// We use $_SERVER variables a lot, so let's use $s instead and save a few bytes
+$s=$_SERVER;
 
 // $c = 'wordpress.zip';
 $c=$w.$z;
 
 // $n = the name of this file on the server
-$n=basename($_SERVER["SCRIPT_NAME"]);
-$h='http://';
+// $n = basename($_SERVER["SCRIPT_NAME"]);
+$n=basename($s["SCRIPT_NAME"]);
 
 // Now we'll delete this file from the server, and cross our fingers that installation goes as planned.
 unlink($n);
@@ -50,6 +54,17 @@ $b->close();
 // Now that everything is in place, we can delete wordpress.zip from the server
 unlink($c);
 
-// Finally, we can kick the user out of this script and over to WordPress setup.
-// Because we have made no changes to wp-config.php we need to direct the user to setup-config.php instead of wp-install.php
-header('Location: '.$h.$_SERVER["HTTP_HOST"].str_replace($n,'wp-admin/setup-config.php',$_SERVER['REQUEST_URI']));
+/**
+	Finally, we can kick the user out of this script and over to WordPress setup.
+
+	Because we have made no changes to wp-config.php we need to direct the user to
+	setup-config.php instead of wp-install.php
+
+	'http://'.$_SERVER["HTTP_HOST"].
+		str_replace(
+			basename($_SERVER["SCRIPT_NAME"]),
+			'wp-admin/setup-config.php',
+			$_SERVER['REQUEST_URI']
+		)
+**/
+header('Location: '.$h.$s["HTTP_HOST"].str_replace($n,'wp-admin/setup-config.php',$s['REQUEST_URI']));
